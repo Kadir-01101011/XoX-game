bc6a575ce301af033613e38bd1b2842bf22314ea
kadir-the-eee <71722234+kadir-the-eee@users.noreply.github.com>
2020-09-28 22:45:03 +0300

    Update XoX.c

--- XoX.c
+++ XoX.c
@@ -21,35 +21,35 @@ void print_table(char table[ROW][COLUMN]) {    // function that prints the table
   }
 }
 
-int which_room() {
+int which_room() {  // function that takes and returns the area of the next move
     int num = 0;
     printf("which room you're gonna type on?_");
     scanf("%d", &num);
     return num;
 }
 
-void move_x (int index, char (*ptrX)[ROW][COLUMN]) {
-    if (index == 1)         *(*(ptrX+0)+2) = 'X';
-    else if (index == 2)    *(*(ptrX+0)+6) = 'X';
-    else if (index == 3)    *(*(ptrX+0)+10) = 'X';
-    else if (index == 4)    *(*(ptrX+3)+2) = 'X';
-    else if (index == 5)    *(*(ptrX+3)+6) = 'X';
-    else if (index == 6)    *(*(ptrX+3)+10) = 'X';
-    else if (index == 7)    *(*(ptrX+5)+2) = 'X';
-    else if (index == 8)    *(*(ptrX+5)+6) = 'X';
-    else if (index == 9)    *(*(ptrX+5)+10) = 'X';
+void move_X (int index, char *ptrX) {
+    if (index == 1)         *(ptrX+0*COLUMN+1) = 'X';
+    else if (index == 2)    *(ptrX+0*COLUMN+5) = 'X';
+    else if (index == 3)    *(ptrX+0*COLUMN+9) = 'X';
+    else if (index == 4)    *(ptrX+2*COLUMN+1) = 'X';
+    else if (index == 5)    *(ptrX+2*COLUMN+5) = 'X';
+    else if (index == 6)    *(ptrX+2*COLUMN+9) = 'X';
+    else if (index == 7)    *(ptrX+4*COLUMN+1) = 'X';
+    else if (index == 8)    *(ptrX+4*COLUMN+5) = 'X';
+    else if (index == 9)    *(ptrX+4*COLUMN+9) = 'X';
 }
 
-void move_O (int index, char *ptrO[ROW][COLUMN]) {
-    if (index == 1)         *(*(ptrO+0)+2) = 'O';
-    else if (index == 2)    *(*(ptrO+0)+6) = 'O';
-    else if (index == 3)    *(*(ptrO+0)+10) = 'O';
-    else if (index == 4)    *(*(ptrO+3)+2) = 'O';
-    else if (index == 5)    *(*(ptrO+3)+6) = 'O';
-    else if (index == 6)    *(*(ptrO+3)+10) = 'O';
-    else if (index == 7)    *(*(ptrO+5)+2) = 'O';
-    else if (index == 8)    *(*(ptrO+5)+6) = 'O';
-    else if (index == 9)    *(*(ptrO+5)+10) = 'O';
+void move_O (int index, char *ptrO) {
+    if (index == 1)         *(ptrO+0*COLUMN+1) = 'O';
+    else if (index == 2)    *(ptrO+0*COLUMN+5) = 'O';
+    else if (index == 3)    *(ptrO+0*COLUMN+9) = 'O';
+    else if (index == 4)    *(ptrO+2*COLUMN+1) = 'O';
+    else if (index == 5)    *(ptrO+2*COLUMN+5) = 'O';
+    else if (index == 6)    *(ptrO+2*COLUMN+9) = 'O';
+    else if (index == 7)    *(ptrO+4*COLUMN+1) = 'O';
+    else if (index == 8)    *(ptrO+4*COLUMN+5) = 'O';
+    else if (index == 9)    *(ptrO+4*COLUMN+9) = 'O';
 }
 
 
@@ -71,13 +71,13 @@ int main() {
         }
     }
     
-  //  char (*patternptr) = &pattern;
-
+    //  char *patternptr = &pattern;
+  
     for (int order = 0; order < 9; order++) {
         int turn = (order % 2) + 1;
         printf("Player %d's turn: \n", turn);
-        if (turn ==1)   move_X(which_room, &pattern);
-        else            move_O(which_room, &pattern);
+        if (turn ==1)   move_X(which_room(), &pattern[0][0]);
+        else            move_O(which_room(), &pattern[0][0]);
         
         print_table(pattern);
     }
