0d7251ffdf63111a40222175dee97674bb2a07e7
kadir-the-eee <71722234+kadir-the-eee@users.noreply.github.com>
2020-10-03 21:01:51 +0300

    Update XoXAI.c

--- XoXAI.c
+++ XoXAI.c
@@ -3,13 +3,6 @@
 
 char room[9];
 
-void how_to_play() {    // function that prints the information about gameplay
-    printf(" 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n\n");
-    printf("Type the room you want to play when it's your turn\n");
-    printf("You can only type on a room that is empty.\n");
-    printf("Player 1: X, Player 2: O\n\n");
-}
-
 void print_table() {    // function that prints the table on which game is played
   printf(" %c | %c | %c \n", room[0], room[1], room[2]);
   printf("-----------\n");
@@ -18,57 +11,41 @@ void print_table() {    // function that prints the table on which game is playe
   printf(" %c | %c | %c \n", room[6], room[7], room[8]);
 }
 
-int which_room() {  // function that takes and returns the area of the next move
+void play(int order) {  // function that takes and returns the area of the next move
     int num = 0;
     printf("Which room you're gonna type on?_");
     scanf("%d", &num);
     
-    if (num <= 9 && num >= 1)
-        return num;
-    else {
-        printf("\nType a number between 1-9");
-        which_room();
+    if (num <= 9 && num >= 1 && room[num-1] == ' ') {
+        if(order == 1)    room[num-1] = 'X';
+        else              room[num-1] = 'O';
     }
-}
-
-void move_X (int index) {
-    if (room[index-1] == ' ')
-        room[index-1] = 'X';
     else {
-        printf("You can't type upon your friends room. Please try again.\n");
-        move_X(which_room());
-    }
-}
-
-void move_O (int index) {   // will be AI
-    if (room[index-1] == ' ')
-        room[index-1] = 'O';
-    else {
-        printf("You can't type upon your friends room. Please try again.\n");
-        move_O(which_room());
+        printf("\nType a number between 1-9 whose room is empty.\n");
+        play(order);
     }
 }
 
 int is_finished() {
-    if (room[0] == room[1] && room[0] == room[2] && room[0] == 'X' ||
-        room[3] == room[4] && room[3] == room[5] && room[3] == 'X' ||
+    if (room[0] == room[1] && room[0] == room[2] && room[0] == 'X' ||   // horizontal
+        room[3] == room[4] && room[3] == room[5] && room[3] == 'X' ||   
         room[6] == room[7] && room[6] == room[8] && room[6] == 'X' ||
-        room[0] == room[3] && room[0] == room[6] && room[0] == 'X' ||
+        room[0] == room[3] && room[0] == room[6] && room[0] == 'X' ||   // vertical
         room[1] == room[4] && room[1] == room[7] && room[1] == 'X' ||
         room[2] == room[5] && room[2] == room[8] && room[2] == 'X' ||
-        room[0] == room[4] && room[0] == room[8] && room[0] == 'X' ||
+        room[0] == room[4] && room[0] == room[8] && room[0] == 'X' ||   // cross
         room[2] == room[4] && room[2] == room[6] && room[2] == 'X') {
             
         printf("Player 1 is the winner!\n");
         return 1;
     }
-    else if (room[0] == room[1] && room[0] == room[2] && room[0] == 'O' ||
+    else if (room[0] == room[1] && room[0] == room[2] && room[0] == 'O' ||  // horizontal
             room[3] == room[4] && room[3] == room[5] && room[3] == 'O' ||
-            room[6] == room[7] && room[6] == room[8] && room[6] == 'O' ||
-            room[0] == room[3] && room[0] == room[6] && room[0] == 'O' ||
+            room[6] == room[7] && room[6] == room[8] && room[6] == 'O' ||   
+            room[0] == room[3] && room[0] == room[6] && room[0] == 'O' ||   // vertical
             room[1] == room[4] && room[1] == room[7] && room[1] == 'O' ||
             room[2] == room[5] && room[2] == room[8] && room[2] == 'O' ||
-            room[0] == room[4] && room[0] == room[8] && room[0] == 'O' ||
+            room[0] == room[4] && room[0] == room[8] && room[0] == 'O' ||   // cross
             room[2] == room[4] && room[2] == room[6] && room[2] == 'O') {
             
         printf("Player 2 is the winner!\n");
@@ -78,24 +55,21 @@ int is_finished() {
 }
 
 int main() {
-    
-    how_to_play();
-    
+        
     for (int n = 0; n < 9; n++) {
         room[n] = ' ';
     }
+    print_table();
     
-    for (int order = 0; order < 9; order++) {
-        int turn = (order % 2) + 1;
+    for (int i = 0; i < 9; i++) {
+        int turn = (i % 2) + 1;
         printf("Player %d's turn: \n", turn);
-        if (turn ==1)   move_X(which_room());
-        else            move_O(which_room());
+        play(turn);
         
         system("clear");
-        how_to_play();
         print_table();
         
-        if (order > 3) {
+        if (i > 3) {
             if (is_finished() == 1) {                
                 printf("Congrats to the winner.\n");
                 return 0;
